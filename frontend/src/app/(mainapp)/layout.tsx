877a8622ad2c51a61b47fe9dd9be6eb5a0e36feb
Selerium <johnadithya008@gmail.com>
2026-08-03 16:42:19 +0400

    updating frontend

--- frontend/src/app/(mainapp)/layout.tsx
+++ frontend/src/app/(mainapp)/layout.tsx
@@ -10,7 +10,7 @@ export default async function MainAppLayout({
   const cookieStore = await cookies();
   const token = cookieStore.get("access_token")?.value;
   if (!token) {
-    redirect("/");
+    redirect("/login");
   }
 
   let role: string | null = null;
--- frontend/src/app/admin/layout.tsx
+++ frontend/src/app/admin/layout.tsx
@@ -9,7 +9,7 @@ export default async function AdminLayout({
   const cookieStore = await cookies();
   const token = cookieStore.get("access_token")?.value;
   if (!token) {
-    redirect("/");
+    redirect("/login");
   }
 
   let role: string | null = null;
