6920a620872d330aa35fe7477f67f0febfc133c8
Selerium <johnadithya008@gmail.com>
2026-08-12 15:08:52 +0400

    fixed small thing with error messages

--- backend/src/middleware/errorHandler.ts
+++ backend/src/middleware/errorHandler.ts
@@ -2,8 +2,12 @@ import express from "express";
 
 const errorHandler = (err, req, res, next) => {
   const status = err.statusCode || 500;
+  const message =
+    status >= 500
+      ? "Internal server error"
+      : err.message || "Internal sever error";
   if (status >= 500) console.error("Unhandled error:", err);
-  res.status(status).json({ error: true, message: "Internal Server Error", data: {} });
+  res.status(status).json({ error: true, message, data: {} });
 };
 
 export default errorHandler;
